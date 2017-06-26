b9055725afe840a06b01e43e60c555f7751d6f3d
Utkarsh Upadhyay <musically.ut@gmail.com>
2017-06-26 22:57:19 +0200

    Raise ValueError if alpha is invalid in LabelSpreading.

--- sklearn/semi_supervised/tests/test_label_propagation.py
+++ sklearn/semi_supervised/tests/test_label_propagation.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from sklearn.utils.testing import assert_equal
 from sklearn.utils.testing import assert_warns
+from sklearn.utils.testing import assert_raises
 from sklearn.utils.testing import assert_no_warnings
 from sklearn.semi_supervised import label_propagation
 from sklearn.datasets import make_classification
@@ -117,3 +118,10 @@ def test_label_propagation():
     expected /= expected.sum(axis=1)[:, np.newaxis]
 
     assert_array_almost_equal(expected, clf.label_distributions_, 4)
+
+
+def test_valid_alpha():
+    assert_raises(ValueError, label_propagation.LabelSpreading, alpha=-0.1)
+    assert_raises(ValueError, label_propagation.LabelSpreading, alpha=0)
+    assert_raises(ValueError, label_propagation.LabelSpreading, alpha=1)
+    assert_raises(ValueError, label_propagation.LabelSpreading, alpha=1.1)
